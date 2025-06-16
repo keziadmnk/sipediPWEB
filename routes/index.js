747baabc9493599131f756577d43ed3a628d4849
NaylaThahira <naylathahirameldian@gmail.com>
2025-06-16 13:17:26 +0700

    nambah views mahasiswa dan admin

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,29 @@ router.get('/', function(req, res, next) {
 
 router.get('/peminjaman', findAllPeminjaman); 
 
+// Route untuk menampilkan Data Buku
+router.get('/databuku', function(req, res, next) {
+  res.render('admin/databuku');  // Render file databuju.ejs
+});
+
+// Route untuk menampilkan Tambah Buku
+router.get('/tambahbuku', function(req, res, next) {
+  res.render('admin/tambahbuku');  // Render file databuju.ejs
+});
+
+// Route untuk menampilkan Koleksi Buku
+router.get('/koleksibuku', function(req, res, next) {
+  res.render('mahasiswa/koleksibuku');  // Render file databuju.ejs
+});
+
+// Route untuk menampilkan Detail Buku
+router.get('/detailbuku', function(req, res, next) {
+  res.render('mahasiswa/detailbuku');  // Render file databuju.ejs
+});
+
+// Route untuk menampilkan Detail Buku
+router.get('/profil', function(req, res, next) {
+  res.render('mahasiswa/profil');  // Render file databuju.ejs
+});
 
 module.exports = router;
