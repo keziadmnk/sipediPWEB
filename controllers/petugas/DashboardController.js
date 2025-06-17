98fdde65472bed4e66487450b7f3545efc5cb4a4
keziadmnk <keziadamanik20@gmail.com>
2025-06-17 13:51:49 +0700

    nambah banyak

---
+++ controllers/petugas/DashboardController.js
@@ -0,0 +1,27 @@
+const { Peminjaman } = require("../../models/PeminjamanModel");
+
+const findStatusStatistik = async (req, res) => {
+  try {
+    const totalpeminjaman = await Peminjaman.count();
+    const totaldipinjam = await Peminjaman.count({
+      where: { status_peminjaman: "Dipinjam" },
+    });
+    const totaldikembalikan = await Peminjaman.count({
+      where: { status_peminjaman: "Dikembalikan" },
+    });
+    const totalterlambat = await Peminjaman.count({
+      where: { status_peminjaman: "Terlambat" },
+    });
+    res.render("petugas/dashboard", {
+      totalpeminjaman,
+      totaldipinjam,
+      totaldikembalikan,
+      totalterlambat,
+    });
+  } catch (error) {
+    console.error("Error fetching dashboard stats:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { findStatusStatistik }
--- routes/PetugasRoute.js
+++ routes/PetugasRoute.js
@@ -2,12 +2,19 @@ const express = require('express');
 const { findAllPeminjaman, findDetailPeminjaman } = require('../controllers/petugas/PeminjamanController');
 const findAllPengembalian = require('../controllers/petugas/PengembalianController');
 const findAllDenda = require('../controllers/petugas/DendaController');
+const { findStatusStatistik } = require('../controllers/petugas/DashboardController');
 const router = express.Router();
 
+router.get('/dashboard', findStatusStatistik);
 router.get('/peminjaman', findAllPeminjaman); 
 router.get('/pengembalian', findAllPengembalian); 
 router.get('/denda', findAllDenda); 
 router.get('/detailpeminjaman/:id_peminjaman', findDetailPeminjaman)
+
+router.get('/profil', function(req, res, next) {
+  res.render('petugas/profil');  
+});
+
 
 router.get('/detailpeminjaman', function(req, res, next) {
   res.render('petugas/detailpeminjaman');  
